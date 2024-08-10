5a2b3cb2e8c3b83fb4548d1451495c6a71ab9309
mehdi80 <mk2954751@gmail.com>
2024-08-10 11:04:42 +0330

    creat header component and navbar

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RecipeItemComponent} from "./recipe-item/recipe-item.component";
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RecipeItemComponent
+  ],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
