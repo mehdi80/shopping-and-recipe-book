2da9475714b365da80db670a5db43e41fb7d023f
mehdi80 <mk2954751@gmail.com>
2024-08-10 14:52:23 +0330

    create local recipe list

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
 import {RecipeListComponent} from "./recipe-list/recipe-list.component";
 
+
 @Component({
   selector: 'app-recipes',
   standalone: true,
