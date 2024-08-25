d0024557c0bb77aa45d5b348298ff96bc8dfbd7f
mehdi80 <mk2954751@gmail.com>
2024-08-25 17:55:48 +0330

    creating ingredient model and updating shopping list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import {ShoppingEditComponent} from "./shopping-edit/shopping-edit.component";
+import {Ingredient} from "../shared/ingredient.model";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
   imports: [
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    NgForOf
   ],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
-
+ingredients:Ingredient[] = [
+  new Ingredient('Apples',5),
+  new Ingredient('Tomatoes',10),
+];
 }
